869f2485c6d86badcaf17d5a77034498d289fb9c
kanchanr2208 <267316562+kanchanr2208@users.noreply.github.com>
2026-07-30 10:05:41 +0000

    Make search input field functional Add regex pattern in HTML to only allow letters, numbers and space Modify event listener to account for custom error msgs for 1. minlength 2. regex pattern mismatch 3. escape key Simulate form submission and trigger validations when Enter is pressed

--- script.js
+++ script.js
@@ -2,12 +2,28 @@ const inputField = document.querySelector(".search-input");
 
 
 inputField.addEventListener("keydown" , (e) => {
-    let cleanText = e.target.value.trim()           //This removes spaces from the beginning or end of the string
+    
  
-    if (e.key === "Enter") {
-        console.log("Enter Pressed")
-        e.target.value = "";
+    if(e.key === "Escape") {
+        e.target.blur();    //This returns the search bar into the default state, as though exiting the input field. but the text doesnt disappear
+        console.log("Exited search bar")
+    } else if (e.key === "Enter") {
+        let cleanText = e.target.value.trim()           //This removes spaces from the beginning or end of the string
+        if (cleanText.length === 0) {
+            return;
+        } else if (e.target.validity.patternMismatch) {
+            e.target.setCustomValidity("Only letters, numbers, and spaces are allowed. Please delete and try again")
+            e.target.reportValidity()
+        }  else if (cleanText.length < 3) {
+            e.target.setCustomValidity("Search must be at least 3 characters.") //This is the custom message being set
+            e.target.reportValidity()       //This is instructing the browser to display the custom error message
+        } else {
+            e.target.setCustomValidity(""); //this is to remove the custom error message
+            console.log("Form submitted: " + cleanText)
+            e.target.value = "";
+        }
     } else {
-        console.log("Wrong Key Bro")
+        e.target.setCustomValidity(""); 
     }
+
 })
