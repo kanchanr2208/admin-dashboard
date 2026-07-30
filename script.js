74d8518baab4e16e4230502a7db5123a7526c517
kanchanr2208 <267316562+kanchanr2208@users.noreply.github.com>
2026-07-30 08:03:42 +0000

    Modify CSS and HTML for Header Section Download profile photo as per design requirements Add length validation to search input field Add CSS to match search input field design Change HTML structure and add CSS to search bat row to align with design Add SVG and photos to header section Add CSS flex details to align header profile section with design Add basic event listerner to search input field

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Yo")
+const inputField = document.querySelector(".search-input");
+
+
+inputField.addEventListener("keydown" , (e) => {
+    let cleanText = e.target.value.trim()           //This removes spaces from the beginning or end of the string
+ 
+    if (e.key === "Enter") {
+        console.log("Enter Pressed")
+        e.target.value = "";
+    } else {
+        console.log("Wrong Key Bro")
+    }
+})
